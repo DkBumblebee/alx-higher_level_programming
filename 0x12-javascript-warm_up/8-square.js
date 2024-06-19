7e24515a91fc857ca178fa415797f94bcf656189
Boitumelo Maswanganyi <bee@localhost.localdomain>
2024-06-19 16:30:42 +0200

    Task 8.1

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined || process.argv[2] === isNaN(process.argv[2])) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   const num = Number(process.argv[2]);
